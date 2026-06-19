fa100db4c2dca3bb98ad9c36c20684730a9918ab
userxspy <userxspy@gmail.com>
2026-06-19 15:19:44 +0530

    Update admin_routes.py

--- web/admin_routes.py
+++ web/admin_routes.py
@@ -3,7 +3,8 @@ from web.login_routes import login_routes
 from web.dashboard_routes import dashboard_routes
 from web.stats_routes import stats_routes
 from web.profile_routes import profile_routes
-from web.actor_routes import actor_routes  # <-- नया एक्टर रूट इम्पोर्ट किया
+from web.actor_routes import actor_routes  
+from web.reels_routes import reels_routes  # 🎬 <-- नया रील्स रूट इम्पोर्ट किया
 
 # एक मास्टर राउट टेबल डिफाइन करें
 admin_routes = web.RouteTableDef()
@@ -14,5 +15,6 @@ def register_admin_components(app: web.Application):
     app.add_routes(dashboard_routes)
     app.add_routes(stats_routes)
     app.add_routes(profile_routes)
-    app.add_routes(actor_routes)       # <-- एक्टर रूट्स को सर्वर इंजन में सिंक किया
+    app.add_routes(actor_routes)       
+    app.add_routes(reels_routes)       # 🎬 <-- रील्स रूट्स को सर्वर इंजन में सिंक किया
     app.add_routes(admin_routes)
